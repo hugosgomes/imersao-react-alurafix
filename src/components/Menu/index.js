b40edb38046e562b9b4839f8e3a9011fc551cf8a
Hugo Gomes <hdsg0807@gmail.com>
2020-07-28 01:19:19 -0300

    Primeira aula completa

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Logo from '../../assets/Logo.png';
 import './Menu.css';
+import Button from '../Button';
 
 const Menu = () => {
   return (
     <nav className="Menu">
       <img className="Logo" src={Logo} alt="Logo AluraFix"/>
+      <Button as="a" className="ButtonLink" href="/">
+        Novo Vídeo
+      </Button>
     </nav>
   );
 }
